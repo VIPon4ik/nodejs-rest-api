b941500fcea7c001e2132ffadbb48b80cdc4c0de
VIPon4ik <umniyd@gmail.com>
2024-01-10 15:28:12 +0200

    Made controller

---
+++ controllers/contacts.js
@@ -0,0 +1,69 @@
+const contacts = require("../../models");
+const HttpError = require("../helpers");
+const contactsValidationSchema = require("../schemas");
+
+const getAll = async (req, res, next) => {
+    try {
+        const result = await contacts.listContacts();
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = contacts.getContactById(id);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const add = async (req, res, next) => {
+    try {
+        const { error } = contactsValidationSchema(req.body);
+        if (error) {
+            throw HttpError(403, "Validation error");
+        }
+        const result = await contacts.addContact(req.body);
+        res.status(201).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const deleteById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await contacts.removeContact(id);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateById = async (req, res, next) => {
+    try {
+        const { error } = contactsValidationSchema(req.body);
+
+        if (error) {
+            throw HttpError(403, "Validation error");
+        }
+
+        const { id } = req.params;
+        const result = contacts.updateContact(id, req.body);
+        res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    getAll,
+    getById,
+    add,
+    deleteById,
+    updateById,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,65 +1,16 @@
 const express = require("express");
-const contacts = require("../../models");
-const HttpError = require("../../helpers");
-const contactsValidationSchema = require("../../schemas");
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-    try {
-        const result = await contacts.listContacts();
-        res.status(200).json(result);
-    } catch (error) {
-        next(error);
-    }
-});
+const ctrl = require("../../controllers/contacts");
 
-router.get("/:contactId", async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const result = contacts.getContactById(id);
-        res.status(200).json(result);
-    } catch (error) {
-        next(error);
-    }
-});
+router.get("/", ctrl.getAll());
 
-router.post("/", async (req, res, next) => {
-    try {
-        const { error } = contactsValidationSchema(req.body);
-        if (error) {
-            throw HttpError(403, "Validation error");
-        }
-        const result = await contacts.addContact(req.body);
-        res.status(201).json(result);
-    } catch (error) {
-        next(error);
-    }
-});
+router.get("/:contactId", ctrl.getById());
 
-router.delete("/:contactId", async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const result = await contacts.removeContact(id);
-        res.status(200).json(result);
-    } catch (error) {
-        next(error);
-    }
-});
+router.post("/", ctrl.add());
 
-router.put("/:contactId", async (req, res, next) => {
-    try {
-        const { error } = contactsValidationSchema(req.body);
+router.delete("/:contactId", ctrl.deleteById());
 
-        if (error) {
-            throw HttpError(403, "Validation error");
-        }
-
-        const { id } = req.params;
-        const result = contacts.updateContact(id, req.body);
-        res.status(200).json(result);
-    } catch (error) {
-        next(error);
-    }
-});
+router.put("/:contactId", ctrl.updateById());
 
 module.exports = router;
