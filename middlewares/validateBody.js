0da3a88a4776cf864231700e817dfbb0cd68f3dc
VIPon4ik <umniyd@gmail.com>
2024-01-27 15:06:55 +0200

    Refactored validateBody middleware

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,9 +3,10 @@ const { HttpError } = require('../helpers');
 const validateBody = schema => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
+    const { message } = error?.details[0] ?? 'Not valid data';
 
     if (error) {
-        next(HttpError(400, "Missing required field"));
+      next(HttpError(400, message));
     }
 
     next()
